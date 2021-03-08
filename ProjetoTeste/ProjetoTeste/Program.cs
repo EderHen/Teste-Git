9c5bb661fbb86a293eed5256c5bb55624bf9b229
Eder Hen <eder.escolaro@gmail.com>
2021-03-08 15:39:02 -0300

    Added some random things

--- ProjetoTeste/ProjetoTeste/Program.cs
+++ ProjetoTeste/ProjetoTeste/Program.cs
@@ -6,9 +6,12 @@ namespace ProjetoTeste
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("-------------------------------");
             Console.WriteLine("Hello World!");
             Console.WriteLine("How are you in this day?");
             Console.WriteLine("Have a good day!");
+            Console.WriteLine("Bye!");
+            Console.WriteLine("-------------------------------------");
 
             Console.WriteLine();
         }
